f0fbea7ba5cf179ad1b917a773f9c6b4055e971d
keithburgie <krburgie@gmail.com>
2023-10-22 17:28:28 -0400

    server is serving data.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 /**
  * The Server Can be configured and created here...
- * 
+ *
  * You can find the JSON Data file here in the Data module. Feel free to impliment a framework if needed.
  */
 
@@ -19,26 +19,55 @@
     ]
 }
 */
-const data      = require('./data');
-const http      = require('http');
-const hostname  = 'localhost';
-const port      = 3035;
+const data = require("./data");
+const http = require("http");
+const hostname = "localhost";
+const port = 3035;
 
-/** 
+/**
  * Start the Node Server Here...
- * 
- * The http.createServer() method creates a new server that listens at the specified port.  
- * The requestListener function (function (req, res)) is executed each time the server gets a request. 
+ *
+ * The http.createServer() method creates a new server that listens at the specified port.
+ * The requestListener function (function (req, res)) is executed each time the server gets a request.
  * The Request object 'req' represents the request to the server.
  * The ServerResponse object 'res' represents the writable stream back to the client.
  */
-http.createServer(function (req, res) {
-    // .. Here you can create your data response in a JSON format
-    
-    
-    res.write("Response goes in here..."); // Write out the default response
-    res.end(); //end the response
-}).listen( port );
 
+/**
+ * Suggestion: install expressjs:
+ * ```
+ * const express = require('express');
+const app = express();
+const port = 3035;
+const data = require('./data');
+
+app.get('/products/search', (req, res) => {
+    const query = req.query.query.toLowerCase();
+    const results = data.filter(product => 
+        product.name.toLowerCase().includes(query) ||
+        product.tags.some(tag => tag.toLowerCase().includes(query))
+    );
+    res.json(results);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
+ * ```
+ */
+
+http
+  .createServer(function (req, res) {
+    // .. Here you can create your data response in a JSON format
+
+    // Set the header to inform the client we're sending JSON
+    res.setHeader("Content-Type", "application/json");
+
+    // Send the data as a JSON string
+    res.write(JSON.stringify(data));
+
+    res.end(); //end the response
+  })
+  .listen(port);
 
 console.log(`[Server running on ${hostname}:${port}]`);
